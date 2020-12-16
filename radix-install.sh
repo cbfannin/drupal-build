738b3fe7b6bc6cd2dc471c55b646dd6b2f0e8e85
Charles Fannin <cbfannin@gmail.com>
2020-12-16 16:43:18 -0500

    update drush versions for radix install

--- radix-install.sh
+++ radix-install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo -e "\n${blue}${white}\n\nInstalling Radix and creating subtheme ...\n${reset}"
-sed -i 's+"drush/drush": "^10.2"+"drush/drush": "^8.0.0"+g' composer.json
+sed -i 's+"drush/drush": "^10.3"+"drush/drush": "^8.0.0"+g' composer.json
 lando composer update
-lando composer require drupal/radix
+lando composer require drupal/components drupal/radix
 lando drush en components radix -y
 subtheme="${projname}_subtheme"
-lando drush radix "${subtheme}"
+lando drush --include="themes/contrib/radix" radix:create "${subtheme}"
 lando drush en ${subtheme} -y
 lando drush config-set system.theme default ${subtheme} -y
 cd ${projpath}/${projname}/web/themes/${subtheme}
@@ -16,6 +16,6 @@ echo -e "\n${blue}${white}\n\nPerforming automatic audit fix ...\n${reset}"
 lando npm audit fix
 lando npm run dev
 cd ${projpath}/${projname}
-sed -i 's+"drush/drush": "^8.0.0"+"drush/drush": "^10.2"+g' composer.json
+sed -i 's+"drush/drush": "^8.0.0"+"drush/drush": "^10.3"+g' composer.json
 lando composer update
 lando drush cr
