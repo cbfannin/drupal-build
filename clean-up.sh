da6fe9834576ccad2b11851500a17b56ea345315
Charles Fannin <cbfannin@gmail.com>
2020-12-17 16:16:34 -0500

    rm commands for deleting watfaults as it was no longer necessary

--- clean-up.sh
+++ clean-up.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo -e "${blue}${white}\n\nPerforming clean up ...\n${reset}\n"
-rm -Rf wat-faults
-composer remove drupal/core-project-message
+lando composer remove drupal/core-project-message
 
 echo -e "\n${blue}${white}Finally done!\nSite Login Information:${reset}"
 echo -e "log in url: ${green}http://${projname}.lndo.site/user${reset}\nusername: ${green}superadmin${reset}\npassword: ${green}admin${reset}"
