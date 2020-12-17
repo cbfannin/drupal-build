11a19938a0d5bc23a2400120771f7837b81bf603
Charles Fannin <cbfannin@gmail.com>
2020-12-17 15:45:38 -0500

    updates where to get team defaults from; currently assumes this build script is stored at ~/bin/drupal-build/

--- site-install.sh
+++ site-install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 cd ${projpath}/${projname}
-echo -e "\n${blue}${white}\n\nCloning Team dot files for project ...\n${reset}"
-git clone git@itsdevrepo.eastus.cloudapp.azure.com:wcrum/wat-faults.git
-cp wat-faults/d8.lando.yml ./.lando.yml
-cp wat-faults/.gitignore .
+echo -e "\n${blue}${white}\n\nCopying team dot files for project ...\n${reset}"
+cp ~/bin/drupal-build/defaults/.lando.yml .
+cp ~/bin/drupal-build/defaults/.gitignore .
 sed -i "s/<project name>/${projname}/g" .lando.yml
 echo -e "\n${blue}${white}\n\nStarting Lando ...\n${reset}"
 lando start
 echo -e "\n${blue}${white}\n\nPerforming Site Install ...\n${reset}"
-lando composer require drupal/console:~1.0 --prefer-dist --optimize-autoloader
-lando composer require drush/drush
 
 lando drush si standard -y --db-url=mysql://drupal8:drupal8@database/${projname} \
 --site-name="${projname}"  \
