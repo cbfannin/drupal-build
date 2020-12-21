e5801e080fce63c0523827bd6ad988f93833ab6f
Charles Fannin <cbfannin@gmail.com>
2020-12-21 15:14:22 -0500

    sets drupal version for composer creation

--- create-project.sh
+++ create-project.sh
@@ -2,6 +2,6 @@
 cd ${projpath}
 echo "${blue}${white}What is the name of your project?${reset}"
 read projname
-echo "${blue}${white}Creating ${reset}${green}${white}${projname} ${reset}${blue}${white}Drupal 8 Composer project at ${green}${white}${projpath}/${projname}.${reset}"
-composer create-project drupal/recommended-project ${projname} -n
+echo "${blue}${white}Creating ${reset}${green}${white}${projname} ${reset}${blue}${white}Drupal Composer project at ${green}${white}${projpath}/${projname}.${reset}"
+composer create-project drupal/recommended-project:8.9.11 ${projname} -n
 cd ${projname}
