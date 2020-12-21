fd74d3924578c2ae43d303c69b656c66ad41c634
Charles Fannin <cbfannin@gmail.com>
2020-12-21 17:22:52 -0500

    fixes kint install and enable

--- developer-modules.sh
+++ developer-modules.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo -e "\n${blue}${white}\n\nDownloading developer modules ...\n${reset}"
-lando composer require drupal/devel
+lando composer require \
+	drupal/devel \
+	kint-php/kint
 
 echo -e "\n${blue}${white}\n\nEnabling developer modules ...\n${reset}"
 lando drush en devel -y
 lando drush en devel_generate -y
-lando drush en kint -y
 lando drush en webprofiler -y
+
