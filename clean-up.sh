f2ce07ef23bcd19644f37b76a77b2b0f0faa11b6
Charles Fannin <cbfannin@gmail.com>
2021-04-12 18:05:34 -0400

    replaces radix with boostrap4 theme

--- clean-up.sh
+++ clean-up.sh
@@ -5,4 +5,3 @@ lando composer remove drupal/core-project-message
 
 echo -e "\n${blue}${white}Finally done!\nSite Login Information:${reset}"
 echo -e "log in url: ${green}http://${projname}.lndo.site/user${reset}\nusername: ${green}superadmin${reset}\npassword: ${green}admin${reset}"
-echo -e "${red}${white}\nIMPORTANT! If you intend to do any theming, don't forget to run lando npm run watch in /web/themes/$subtheme in a separate terminal.\n${reset}"
--- theme-install.sh
+++ theme-install.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
 echo -e "\n${blue}${white}\n\nInstalling Theme ...\n${reset}"
-sed -i 's+"drush/drush": "^10.3"+"drush/drush": "^8.0.0"+g' composer.json
-lando composer update
-lando composer require drupal/components drupal/radix
-lando drush en components radix -y
-subtheme="${projname}_subtheme"
-lando drush --include="themes/contrib/radix" radix:create "${subtheme}"
-lando drush en ${subtheme} -y
-lando drush config-set system.theme default ${subtheme} -y
-cd ${projpath}/${projname}/web/themes/${subtheme}
-lando npm install
-sed -i 's+http://drupal.local+http://'${projname}'.lndo.site+g' webpack.mix.js
-echo -e "\n${blue}${white}\n\nPerforming automatic audit fix ...\n${reset}"
-lando npm audit fix
-lando npm run dev
-cd ${projpath}/${projname}
-sed -i 's+"drush/drush": "^8.0.0"+"drush/drush": "^10.3"+g' composer.json
-lando composer update
+lando composer require drupal/bootstrap4:^2.1
+lando drush theme:enable bootstrap4
+lando drush config-set system.theme default bootstrap4 -y
 lando drush cr
