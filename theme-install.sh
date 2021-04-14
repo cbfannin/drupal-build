3fa162c9919919db4e068a5d83cd1c92e7c7969c
Jason Bugiada <jason.bugiada@chattanoogastate.edu>
2021-04-14 09:18:42 -0400

    Added subtheme

--- theme-install.sh
+++ theme-install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo -e "\n${blue}${white}\n\nInstalling Theme ...\n${reset}"
+echo -e "\n${blue}${white}\n\nInstalling Bootstrap4 Theme ...\n${reset}"
 lando composer require drupal/bootstrap4:^2.1
-lando drush theme:enable bootstrap4
-lando drush config-set system.theme default bootstrap4 -y
+
+echo -e "\n${blue}${white}\n\nInstalling Chatt State Bootstrap4 Subtheme ...\n${reset}"
+mkdir web/themes/custom
+git clone git@watlab.eastus.cloudapp.azure.com:jbugiada/cscc_theme.git web/themes/custom
+lando drush theme:enable bootstrap4 cscc_theme
+lando drush config-set system.theme default cscc_theme -y
 lando drush cr
