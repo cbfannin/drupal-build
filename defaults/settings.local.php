e8067da4c452de0acb061e080dc756c81d3098c1
Bill Crum <wacrum@gmail.com>
2021-05-27 12:49:05 -0400

    Fixes settings.local.php

--- defaults/settings.local.php
+++ defaults/settings.local.php
@@ -88,8 +88,16 @@
  * ];
  * @endcode
  */
-$databases = [];
-
+$databases['default']['default'] = array (
+  'database' => 'drupal8',
+  'username' => 'drupal8',
+  'password' => 'drupal8',
+  'prefix' => '',
+  'host' => 'database',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
 /**
  * Customizing database settings.
  *
@@ -795,13 +803,3 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-$databases['default']['default'] = array (
-  'database' => 'drupal8',
-  'username' => 'drupal8',
-  'password' => 'drupal8',
-  'prefix' => '',
-  'host' => 'database',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
