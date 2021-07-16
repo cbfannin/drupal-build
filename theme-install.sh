1db4147ecfdfda73bfbcc7a5cd0b91ce45e4536b
Jason Bugiada <jason.bugiada@chattanoogastate.edu>
2021-07-16 15:37:06 -0400

    Removed gin theme

--- theme-install.sh
+++ theme-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo -e "\n${blue}${white}\n\nInstalling Bootstrap4 Theme ...\n${reset}"
-lando composer require drupal/bootstrap4:^2.1
+lando composer require drupal/bootstrap4:^2.1.13 
 
 echo -e "\n${blue}${white}\n\nInstalling Chatt State Bootstrap4 Subtheme ...\n${reset}"
 mkdir web/themes/custom
 git clone git@watlab.eastus.cloudapp.azure.com:jbugiada/cscc_theme.git web/themes/custom/cscc_theme
-lando drush theme:enable bootstrap4 cscc_theme
+lando drush theme:enable bootstrap4 cscc_theme 
 lando drush config-set system.theme default cscc_theme -y
 lando drush cr
