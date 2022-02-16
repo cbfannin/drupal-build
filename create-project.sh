58a26e861564ceea2be0535dc5dcc3876a973d2b
Charles Fannin <cbfannin@gmail.com>
2022-02-15 19:49:40 -0500

    removes drupal version from composer create

--- create-project.sh
+++ create-project.sh
@@ -3,5 +3,5 @@ cd ${projpath}
 echo "${blue}${white}What is the name of your project?${reset}"
 read projname
 echo "${blue}${white}Creating ${reset}${green}${white}${projname} ${reset}${blue}${white}Drupal Composer project at ${green}${white}${projpath}/${projname}.${reset}"
-composer create-project drupal/recommended-project:^9.2 ${projname} -n
+composer create-project drupal/recommended-project ${projname} -n
 cd ${projname}
