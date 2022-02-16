3c09a127b043e8f1f682d74a09eab7de3b9335f9
Charles Fannin <cbfannin@gmail.com>
2022-02-15 20:10:45 -0500

    adds composer command to remove project message from config

--- clean-up.sh
+++ clean-up.sh
@@ -2,6 +2,7 @@
 
 echo -e "${blue}${white}\n\nPerforming clean up ...\n${reset}\n"
 lando composer remove drupal/core-project-message
+lando composer config --unset extra.drupal-core-project-message
 
 echo -e "\n${blue}${white}Finally done!\nSite Login Information:${reset}"
 echo -e "log in url: ${green}http://${projname}.lndo.site/user${reset}\nusername: ${green}superadmin${reset}\npassword: ${green}admin${reset}"
