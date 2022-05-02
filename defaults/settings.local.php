8f517f84f59c6826023afdd71a71bf7de57cee4a
Charles Fannin <cbfannin@gmail.com>
2022-05-02 19:45:18 -0400

    include theme debugg settings

--- defaults/settings.local.php
+++ defaults/settings.local.php
@@ -13,4 +13,12 @@ $databases['default']['default'] = [
 
 $settings['file_private_path'] = '/app/private';
 $settings['hash_salt'] = 'local';
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+// Change kint depth_limit setting.
+if (class_exists('Kint')) {
+  // Set the depth_limit to prevent out-of-memory.
+  \Kint::$depth_limit = 4;
+}
 
